09d528dd938be85e4b8f5e9d621d462d803f45d1
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-08-23 21:40:10 -0500

    Se Crea Clase Empleado, MovimientoDinero y se modifica el verEmpresas html en la parte del Getmapping no se logra conexion a la base de datos, se realizan comentarios en algunas lineas

--- src/main/java/com/udea/controller/EmpresaController.java
+++ src/main/java/com/udea/controller/EmpresaController.java
@@ -1,17 +1,32 @@
 package com.udea.controller;
 
 import com.udea.models.Empresa;
+import com.udea.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+import java.util.List;
+
+
 @Controller
 public class EmpresaController {
 
-    @GetMapping("/")
-    public String inicio(Model model){
+    //@Autowired //*
+    //EmpresaService empresaService; //*
+
+    @GetMapping({"/","/VerEmpresas"})//Cambie el GeTmapping
+
+    public String viewEmpresas (Model model){
+        //List<Empresa> listaEmpresa=empresaService.getAllEmpresas();
+        //model.addAttribute("emplist",listaEmpresas);
+
         var empresa = new Empresa("UDEA","CALLE 72 A # 435","4849401","800106404");
         model.addAttribute("emp",empresa);
+
         return "verEmpresas";
     }
 }
