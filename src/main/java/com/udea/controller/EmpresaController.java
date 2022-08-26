a7a3373f590b309877b74b2b9022a02a9567f786
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2022-08-25 22:10:24 -0500

    Terminamos el crud basico de nuestra entidad Empresa

--- src/main/java/com/udea/controller/EmpresaController.java
+++ src/main/java/com/udea/controller/EmpresaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -37,11 +38,37 @@ public class EmpresaController {
 
     @PostMapping("/GuardarEmpresa")
     public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
-
         if(empresaService.saveOrUpdateEmpresa(emp)==true){
         return "redirect:/VerEmpresas";
     }
         return "redirect:/AgregarEmpresa";
+    }
+
+    @GetMapping("/EditarEmpresa/{id}")
+    public String editarEmpresa(Model model, @PathVariable Integer id){
+        Empresa emp=empresaService.getEmpresaById(id);
+        //Creamos un atributo para el modelo, que se llame igualmente emp y es el que ira al html para llenar o alimentar campos
+        model.addAttribute("emp",emp);
+        return "editarEmpresa";
+
+    }
+
+    @PostMapping("/ActualizarEmpresa")
+    public String updateEmpresa(Empresa emp){
+        if(empresaService.saveOrUpdateEmpresa(emp)==true){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/EditarEmpresa";
+    }
+
+    @GetMapping("/EliminarEmpresa/{id}")
+    public String eliminarEmpresa(@PathVariable Integer id){
+        try{
+            empresaService.deleteEmpresa(id);
+        }catch (Exception e){
+            return "redirect:/VerEmpresas";
+        }
+        return "redirect:/VerEmpresas";
     }
 
 }
